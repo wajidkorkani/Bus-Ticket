6c923e715a210b0c6a65715c7f572f8ea4ebb78f
wajidkorkani <errormrcoder@gmail.com>
2025-03-27 17:17:14 +0500

    It will display your location.

--- BusTicket/Core/views.py
+++ BusTicket/Core/views.py
@@ -1,3 +1,12 @@
+import geocoder
 from django.shortcuts import render
 
-# Create your views here.
+# View to fetch and display the location
+def home(request):
+    g = geocoder.ip('me')  # Fetch location based on the current IP address
+    latitude, longitude = g.latlng if g.latlng else (None, None)
+    
+    return render(request, 'index.html', {
+        'latitude': latitude,
+        'longitude': longitude
+    })
